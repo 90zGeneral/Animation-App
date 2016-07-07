13b46140475492f138348dd7fa00cbd83de331e8
Roydon Jeffrey <brushdam14823@gmail.com>
2016-07-06 20:33:27 -0400

    Completion with animation working seamlessly on button click and allowing users to start and stop the animation

--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -9,10 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer = NSTimer()
+    var counter = 1
+    var animationStopped = true
+    
+    @IBOutlet var animatedImage: UIImageView!
+    
+    @IBOutlet var animateButton: UIButton!
+    
+    @IBAction func animate(sender: AnyObject) {
+        
+        if animationStopped {
+        
+            timer = NSTimer.scheduledTimerWithTimeInterval(0.2, target: self, selector: #selector(self.doAnimation), userInfo: nil, repeats: true)
+            
+            self.animateButton.setTitle("Stop", forState: .Normal)
+            self.animateButton.setTitleColor(UIColor.redColor(), forState: .Normal)
+            
+            animationStopped = false
+         
+        }else {
+            
+            timer.invalidate()
+            
+            self.animateButton.setTitle("Animate", forState: .Normal)
+            self.animateButton.setTitleColor(UIColor.greenColor(), forState: .Normal)
+            
+            animationStopped = true
+            
+        }
+        
+    }
+    
+    func doAnimation() {
+        
+        if counter == 5 {
+            counter = 1
+        }else {
+            counter += 1
+        }
+        
+        animatedImage.image = UIImage(named: "frame\(counter).png")
+
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.animateButton.setTitleColor(UIColor.greenColor(), forState: .Normal)
     }
 
     override func didReceiveMemoryWarning() {
